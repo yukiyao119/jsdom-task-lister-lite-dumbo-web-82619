72a45ede0e4dac4f74226376c398a49e6dbda10e
yukiyao119 <ukiyao@hotmail.com>
2019-10-09 09:24:48 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+let newForm = document.querySelector("#create-task-form")
+console.log(newForm)
+
+let listOfTaskUL = document.querySelector("#tasks")
+
+
+newForm.addEventListener("submit", function(evt){
+  evt.preventDefault()
+  
+  let newTask = evt.target.querySelector("input").value
+  console.log(newTask)
+
+  let newTaskLi = document.createElement("li")
+  newTaskLi.innerText = newTask
+  listOfTaskUL.append(newTaskLi)
+  // newTaskLi.setAttribute("data-task, task")
+
+  let deleteTaskButton = document.createElement("button")
+ 
+  deleteTaskButton.innerText = "screw you"
+  newTaskLi.append(deleteTaskButton)
+
+  deleteTaskButton.addEventListener("click", handleDelete)
+  
+  function handleDelete(evt){
+      evt.target.parentNode.remove()
+  }
+
+} );
+
+
+
+
+
+// document.addEventListener("DOMContentLoaded", () => {
+// });
